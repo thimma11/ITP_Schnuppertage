7fe6f902f90fae5a177a610d34572564bb1a14b1
Kevin Hetzendorfer <hetzi.hetzendorfer@gmx.at>
2017-09-28 16:15:20 +0200

    UserView
    basd

---
+++ app/src/components/UserView/AdminView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default class AdminView extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        
+    }
+}
---
+++ app/src/components/UserView/UserView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+export default class UserView extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+    }
+}
