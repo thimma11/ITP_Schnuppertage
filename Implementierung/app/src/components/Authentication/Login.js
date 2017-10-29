fb354c33376f9cffc2e42b141a9aa25b23602a7f
Kevin Hetzendorfer <hetzi.hetzendorfer@gmx.at>
2017-10-29 14:30:17 +0100

    asd
    asd

--- Implementierung/app/src/components/Authentication/Login.js
+++ Implementierung/app/src/components/Authentication/Login.js
@@ -10,6 +10,11 @@ export default class Login extends React.Component {
             username: "",
             password: ""
         }
+
+        this.onPasswordChange = this.onPasswordChange.bind(this);
+        this.onRegisterClick = this.onRegisterClick.bind(this);
+        this.onUsernameChange = this.onUsernameChange.bind(this);
+        this.LoginEvent = this.LoginEvent.bind(this);
     }
 
     render() {
@@ -18,20 +23,20 @@ export default class Login extends React.Component {
                 <div className="form-group row">
                     <label htmlFor="username" className="col-sm-4 col-form-label" >Username</label>
                     <div className="col">
-                        <input type="text" id="username" className="form-control" onChange={(e) => this.onUsernameChange(e)} /> 
+                        <input type="text" id="username" className="form-control" onChange={this.onUsernameChange} /> 
                     </div>
                 </div>
                 <div className="form-group row">
                     <label htmlFor="password" className="col-sm-4 col-form-label" >Password</label>
                     <div className="col">
-                        <input type="password" id="password"  className="form-control" onChange={(e) => this.onPasswordChange(e)} /> 
+                        <input type="password" id="password"  className="form-control" onChange={this.onPasswordChange} /> 
                     </div>
                 </div>
                 <div className="form-group row">
-                    <a href="#" className="col text-muted" onClick={() => this.onRegisterClick()}>Register now!</a>
+                    <a href="#" className="col text-muted" onClick={this.onRegisterClick}>Register now!</a>
                 </div>
                 <div className="form-group row col">
-                    <button  className="form-control" onClick={() => this.LoginEvent()}>Login</button>
+                    <button  className="form-control" onClick={this.LoginEvent}>Login</button>
                 </div>
             </div>
         );
