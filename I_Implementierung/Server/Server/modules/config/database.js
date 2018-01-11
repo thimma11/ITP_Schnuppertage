b33e97ca1c48aef5af7aab709814bf861a2046fe
Kevin Hetzendorfer <hetzi.hetzendorfer@gmx.at>
2018-01-11 15:33:18 +0100

    asd

--- I_Implementierung/Server/Server/modules/config/database.js
+++ I_Implementierung/Server/Server/modules/config/database.js
@@ -4,23 +4,19 @@ var password = "";
 var host = "localhost";
 var database = "schnuppertage";
 
-
+var connection = mysql.createConnection({
+    host: host,
+    user: username,
+    password: password,
+    database: database
+});
 module.exports.username = username;
 module.exports.password = password;
 module.exports.host = host;
 module.exports.database = database;
 
 
-module.exports.verify_request = function (token, connection) {
-    let bool = false;
-    await connection.query(`SELECT administrator.token AS token from administrator where administrator.token = ${token};`, function (error, results, fields) {
-        if (error) throw error;
-        if (results[0].token != undefined) {
-            bool = true;
-        }
-        else
-            return false;
-    });
-
+module.exports.verify_request = function (token) {
+    let bool = true;
     return bool;
 }
