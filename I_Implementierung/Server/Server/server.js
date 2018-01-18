b2917da6cf061647f941bf815fe028b604348f15
Kevin Hetzendorfer <hetzi.hetzendorfer@gmx.at>
2018-01-18 13:57:39 +0100

    asd

--- I_Implementierung/Server/Server/server.js
+++ I_Implementierung/Server/Server/server.js
@@ -13,11 +13,11 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Credentials', false);
 
     // Pass to next layer of middleware
     next();
