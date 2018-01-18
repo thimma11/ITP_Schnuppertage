b42d99cd8201a7ae2480fbd9f9fe6c2d8d47cf07
Kevin Hetzendorfer <hetzi.hetzendorfer@gmx.at>
2018-01-18 14:32:41 +0100

    asd

--- I_Implementierung/Server/Server/modules/api.js
+++ I_Implementierung/Server/Server/modules/api.js
@@ -4,22 +4,18 @@ var subject = require('./api/subject');
 var teacher = require('./api/teacher');
 var lesson = require('./api/lesson');
 var department = require('./api/department/department');
-<<<<<<< HEAD
 var participant = require('./api/participant');
-=======
 var location = require('./api/location/location');
->>>>>>> 867c9060e495a2ccb150d0cce40d9b19e93b54e6
+var event = require('./api/event');
 
 
 router.use('/subjects', subject);
 router.use('/teachers', teacher);
 router.use('/lessons', lesson);
 router.use('/departments', department);
-<<<<<<< HEAD
 router.use('/participants', participant);
-=======
 router.use('/locations', location);
->>>>>>> 867c9060e495a2ccb150d0cce40d9b19e93b54e6
+router.use('/events', event);
 
 router.get('/', (req, res) => {
     res.send("asd");
---
+++ I_Implementierung/Server/Server/modules/api/event.js
@@ -0,0 +1,31 @@
+﻿var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var database_config = require('../config/database');
+
+var connection = mysql.createConnection({
+    host: database_config.host,
+    user: database_config.username,
+    password: database_config.password,
+    database: database_config.database
+});
+
+router.delete('/:id', (req, res) => {
+    let token = req.query.token;
+    if (token != undefined && database_config.verify_request(token)) {
+        let id = req.params.id;
+        connection.query(`DELETE from events WHERE ID = ${req.params.id};`, function (error, results, fields) {
+            if (error) console.log(error);
+            res.json(results);
+        });
+    }
+    else {
+        res.sendStatus(401);
+        res.end();
+    }
+});
+
+
+
+
+module.exports = router;
