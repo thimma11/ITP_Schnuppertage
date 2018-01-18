3df6683a0a4a71e1c2529dad48792998bf159525
ThomasNotebook\Thomas Himmer <tomhimmer@gmail.com>
2018-01-18 14:32:47 +0100

    participants finished

--- I_Implementierung/Server/Server/modules/api/participant.js
+++ I_Implementierung/Server/Server/modules/api/participant.js
@@ -10,13 +10,7 @@ var connection = mysql.createConnection({
     database: database_config.database
 });
 
-
-router.get('/', (req, res) => {
-    connection.query(`SELECT teacher.ID AS id, teacher.contraction from teacher;`, function (error, results, fields) {
-        if (error) throw error;
-        res.json(results);
-    });
-});
+router.get('/', (req, res) => {});
 
 router.post('/', (req, res) => {
     let token = req.query.authToken;
@@ -24,7 +18,8 @@ router.post('/', (req, res) => {
     console.log(req.body);
     if (token != undefined && database_config.verify_request(token)) {
         connection.connect();
-        connection.query(`INSERT INTO participants p (p.firstname, p.lastname, p.phone, p.email, p.school_location, p.school_typ, p.events_id) VALUES ('${req.body.firstname}', '${req.body.lastname}', '${req.body.phone}', '${req.body.email}', '${req.body.school_location}', '${req.body.school_typ}', '${req.body.events_id}');`, function (error, results, fields) {
+        console.log(req.body);
+        connection.query(`INSERT INTO participants (firstname, lastname, phone, email, school_location, school_typ, events_id) VALUES (?, ?, ?, ?, ?, ?, ?)`, [req.body.firstname, req.body.lastname, req.body.phone, req.body.email, req.body.school_location, req.body.school_typ, req.body.events_id], function (error, results, fields) {
             if (error) console.log(error);
             res.json(results);
         });
@@ -37,16 +32,17 @@ router.post('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
     let id = req.params.id;
-    connection.query(`SELECT teachers.ID AS id, teachers.contraction from teachers WHERE teachers.ID = ${id};`, function (error, results, fields) {
+    connection.query(`SELECT * from participants p WHERE p.events_id = ${id};`, function (error, results, fields) {
         if (error) throw error;
         res.json(results);
     });
 });
+
 router.delete('/:id', (req, res) => {
     let token = req.query.token;
     if (database_config.verify_request(token)) {
         let id = req.params.id;
-        connection.query(`DELETE FROM teachers WHERE teachers.ID = ${id};`, function (error, results, fields) {
+        connection.query(`DELETE FROM participants WHERE id = ${id};`, function (error, results, fields) {
             if (error) throw error;
             res.json(results);
         });
@@ -56,8 +52,5 @@ router.delete('/:id', (req, res) => {
         res.end();
     }
 });
-
-
-
 
 module.exports = router;
