d6aab3d3f72b5489af456bc7abbcbf6d9191fba5
Kevin Hetzendorfer <hetzi.hetzendorfer@gmx.at>
2018-01-18 13:36:25 +0100

    asd

--- I_Implementierung/Server/Server/server.js
+++ I_Implementierung/Server/Server/server.js
@@ -7,6 +7,26 @@ var bodyParser = require('body-parser');
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
 // parse application/json
 app.use(bodyParser.json())
 
@@ -20,4 +40,6 @@ app.get('/', (req, res) => {
 app.use('/api', require('./modules/api'));
 
 var server = http.createServer(app);
-server.listen(port);
+server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
