4e251d6d675b96b0af6c04314d3acabe85fc34b8
ThomasNotebook\Thomas Himmer <tomhimmer@gmail.com>
2018-01-18 14:38:31 +0100

    C

--- I_Implementierung/Server/Server/modules/api.js
+++ I_Implementierung/Server/Server/modules/api.js
@@ -5,11 +5,6 @@ var teacher = require('./api/teacher');
 var lesson = require('./api/lesson');
 var department = require('./api/department/department');
 var participant = require('./api/participant');
-<<<<<<< HEAD
-=======
-var location = require('./api/location/location');
-var event = require('./api/event');
->>>>>>> b42d99cd8201a7ae2480fbd9f9fe6c2d8d47cf07
 
 
 router.use('/subjects', subject);
@@ -17,11 +12,6 @@ router.use('/teachers', teacher);
 router.use('/lessons', lesson);
 router.use('/departments', department);
 router.use('/participants', participant);
-<<<<<<< HEAD
-=======
-router.use('/locations', location);
-router.use('/events', event);
->>>>>>> b42d99cd8201a7ae2480fbd9f9fe6c2d8d47cf07
 
 router.get('/', (req, res) => {
     res.send("asd");
