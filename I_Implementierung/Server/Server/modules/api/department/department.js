bab1f7b242f4de432ca255ba292752ae4cb2d007
Kevin Hetzendorfer <hetzi.hetzendorfer@gmx.at>
2018-01-25 15:11:06 +0100

    asd

--- I_Implementierung/Server/Server/modules/api/department/department.js
+++ I_Implementierung/Server/Server/modules/api/department/department.js
@@ -37,14 +37,26 @@ router.get('/:id/locations', (req, res) => {
 });
 router.post('/:id/locations', (req, res) => {
     let id = req.params.id;
-    connection.query(`INSERT INTO timetables (timetables.DEPARTMENTS_ID, timetables.LOCATIONS_ID) VALUES (?, ?);`, [req.body.location_id, id], function (error, results, fields) {
+    var connection2 = mysql.createConnection({
+        host: database_config.host,
+        user: database_config.username,
+        password: database_config.password,
+        database: database_config.database
+    });
+    var connection3 = mysql.createConnection({
+        host: database_config.host,
+        user: database_config.username,
+        password: database_config.password,
+        database: database_config.database
+    });
+    connection.query(`INSERT INTO timetables (timetables.DEPARTMENTS_ID, timetables.LOCATIONS_ID) VALUES (?, ?);`, [id, req.body.location_id], function (error, results, fields) {
         if (error) throw error;
-        connection.query(`INSERT INTO groups(groups.LOCATION_ID, groups.DEPARTMENT_ID) VALUES (?, ?);`, [req.body.location_id, id], function (error, results, fields) {
+        connection2.query(`INSERT INTO groups(groups.LOCATION_ID, groups.DEPARTMENT_ID) VALUES (?, ?);`, [req.body.location_id, id], function (error, results, fields) {
             if (error) throw error;
-            let daytable_id = results[0]["insertId"];
-            connection.query(`INSERT INTO daytables (daytables.DAY_NAME, daytables.GROUPS_ID) VALUES ('MO', ?), ('DI', ?), ('MI', ?), ('DO', ?), ('FR', ?);`, [daytable_id], function (error, results, fields) {
+            let daytable_id = results["insertId"];
+            connection3.query(`INSERT INTO daytables (daytables.DAY_NAME, daytables.GROUPS_ID) VALUES ('MO', ?), ('DI', ?), ('MI', ?), ('DO', ?), ('FR', ?);`, [daytable_id, daytable_id, daytable_id, daytable_id, daytable_id], function (error, results, fields) {
                 if (error) throw error;
-
+                res.json(results);
             });
         });
     });
