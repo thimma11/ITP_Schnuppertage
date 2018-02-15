59bc87b2fe212ab497056c86a980604e0b35774f
ThomasNotebook\Thomas Himmer <tomhimmer@gmail.com>
2018-02-15 14:55:53 +0100

    c

--- I_Implementierung/Server/Server/modules/api/participant.js
+++ I_Implementierung/Server/Server/modules/api/participant.js
@@ -15,9 +15,13 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-    console.log(req.body);
-    connection.query(`INSERT INTO participants (firstname, lastname, phone, email, school_location, school_typ, events_id) VALUES (?, ?, ?, ?, ?, ?, ?)`, [req.body.firstname, req.body.lastname, req.body.phone, req.body.email, req.body.school_location, req.body.school_typ, req.body.events_id], function (error, results, fields) {
+    connection.query('SELECT id FROM events WHERE date=? and departments_id=? and locations_id=?;', [req.body.date, req.body.department_id, req.body.location_id], function (error, results, fields) {
         if (error) console.log(error);
+
+        connection.query(`INSERT INTO participants (firstname, lastname, phone, email, school_location, school_typ, events_id) VALUES (?, ?, ?, ?, ?, ?, ?)`, [req.body.firstname, req.body.lastname, req.body.phone, req.body.email, req.body.school_location, req.body.school_typ, results[0].id], function (error, results, fields) {
+            if (error) console.log(error);
+        });
+
         res.json(results);
     });
 });
