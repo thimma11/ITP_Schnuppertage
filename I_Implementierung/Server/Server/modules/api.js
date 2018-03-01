52f783f533a5feaedb89bf769c865daed8c0e9d6
Kevin Hetzendorfer <hetzi.hetzendorfer@gmx.at>
2018-03-01 14:17:16 +0100

    asd

--- I_Implementierung/Server/Server/modules/api.js
+++ I_Implementierung/Server/Server/modules/api.js
@@ -11,9 +11,6 @@ var group = require('./api/group/group');
 var timetable = require('./api/timetable');
 var daytable = require('./api/daytable/daytable');
 
-var jwt = require('jsonwebtoken');
-var secret = 'itpistcool';
-
 var mysql = require('mysql');
 var database_config = require('./config/database');
 
@@ -22,87 +19,6 @@ var connection = mysql.createConnection({
     user: database_config.username,
     password: database_config.password,
     database: database_config.database
-});
-
-router.post('/authenticate', (req, res) => {
-    console.log("POST");
-    if (req.body.username != undefined && req.body.password != undefined && req.body.username != "" && req.body.password != "") {
-        let mysql = require('mysql');
-        let database_config = require('./modules/config/database');
-
-        let connection = mysql.createConnection({
-            host: database_config.host,
-            user: database_config.username,
-            password: database_config.password,
-            database: database_config.database
-        });
-
-        connection.query('SELECT * FROM admin WHERE admin.USERNAME = ? AND admin.PASSWORD = ?', [req.body.username, req.body.password], function (error, results, fields) {
-            if (error) throw error;
-            if (results) {
-                const payload = {
-                    username: req.body.username
-                };
-                var token = jwt.sign(payload, secret, {
-                });
-
-                // return the information including token as JSON
-                res.json({
-                    success: true,
-                    token: token
-                });
-            }
-        });
-    }
-    else {
-        res.status(200).send({
-            success: false,
-            message: 'No token provided.'
-        });
-    }
-
-});
-
-router.use(function (req, res, next) {
-
-    // check header or url parameters or post parameters for token
-    var token = req.body.authToken || req.query.authToken || req.headers['Authentication'];
-
-    let not_admin_methods = ["GET", "POST", "PUT", "DELETE"];
-
-    // decode token
-    if (token) {
-
-        // verifies secret and checks exp
-        jwt.verify(token, secret, function (err, decoded) {
-            if (err) {
-                return res.json({ success: false, message: 'Failed to authenticate token.' });
-            } else {
-                // if everything is good, save to request for use in other routes
-                req.decoded = decoded;
-                next();
-            }
-        });
-
-    } else {
-        if (not_admin_methods.find((element) => {
-            if (req.method.toUpperCase() == element.toUpperCase())
-                return 1;
-            else
-                return 0;
-        })) {
-            next();
-        }
-        else {
-            // if there is no token
-            // return an error
-            return res.status(403).send({
-                success: false,
-                message: 'No token provided.'
-            });
-        }
-
-    }
 });
 
 router.use('/subjects', subject);
--- I_Implementierung/Server/Server/server.js
+++ I_Implementierung/Server/Server/server.js
@@ -3,6 +3,9 @@ var http = require('http');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken');
+
+app.set('secret', 'itpistcool');
 
 app.use(function (req, res, next) {
 
@@ -35,8 +38,87 @@ app.get('/', (req, res) => {
     console.log("index");
     res.send("Index");
 });
+app.post('/authenticate', (req, res) => {
+    console.log("POST");
+    res.json("asd");
+    /*if (req.body.username != undefined && req.body.password != undefined && req.body.username != "" && req.body.password != "") {
+        let mysql = require('mysql');
+        let database_config = require('./modules/config/database');
 
+        let connection = mysql.createConnection({
+            host: database_config.host,
+            user: database_config.username,
+            password: database_config.password,
+            database: database_config.database
+        });
 
+        connection.query('SELECT * FROM admin WHERE admin.USERNAME = ? AND admin.PASSWORD = ?', [req.body.username, req.body.password], function (error, results, fields) {
+            if (error) throw error;
+            if (results) {
+                const payload = {
+                    username: req.body.username
+                };
+                var token = jwt.sign(payload, app.get('secret'), {
+                });
+
+                // return the information including token as JSON
+                res.json({
+                    success: true,
+                    token: token
+                });
+            }
+        });
+    }
+    else {
+        res.status(200).send({
+            success: false,
+            message: 'No token provided.'
+        });
+    } */
+
+});
+
+app.use(function (req, res, next) {
+
+    // check header or url parameters or post parameters for token
+    var token = req.body.authToken || req.query.authToken || req.headers['Authentication'];
+
+    let not_admin_methods = ["GET", "POST", "PUT", "DELETE"];
+
+    // decode token
+    if (token) {
+
+        // verifies secret and checks exp
+        jwt.verify(token, app.get('secret'), function (err, decoded) {
+            if (err) {
+                return res.json({ success: false, message: 'Failed to authenticate token.' });
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;
+                next();
+            }
+        });
+
+    } else {
+        if (not_admin_methods.find((element) => {
+            if (req.method.toUpperCase() == element.toUpperCase())
+                return 1;
+            else
+                return 0;
+        })) {
+            next();
+        }
+        else {
+            // if there is no token
+            // return an error
+            return res.status(403).send({
+                success: false,
+                message: 'No token provided.'
+            });
+        }
+
+    }
+});
 
 app.use('/api', require('./modules/api'));
 
