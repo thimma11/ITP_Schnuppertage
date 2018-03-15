bdb967e884bfa3576c833cc6addabefc82d89d65
Kevin Hetzendorfer <hetzi.hetzendorfer@gmx.at>
2018-03-15 14:39:01 +0100

    asd

--- I_Implementierung/Server/Server/server.js
+++ I_Implementierung/Server/Server/server.js
@@ -87,9 +87,6 @@ app.post('/authenticate', (req, res) => {
     
 
 });
-
-app.use('/api', require('./modules/api'));
-
 app.use(function (req, res, next) {
 
     // check header or url parameters or post parameters for token
@@ -131,6 +128,9 @@ app.use(function (req, res, next) {
 
     }
 });
+app.use('/api', require('./modules/api'));
+
+
 
 var server = http.createServer(app);
 server.listen(port, () => {
